cf5bd00ced6982ec5660f104ef0a7f2b9a381d6b
Sam S Sun <sam.s.sun@pwc.com>
2020-05-22 11:27:35 +0800

    first commit

---
+++ src/main/java/com/netsuite/webservices/samples/WsClientFactory.java
@@ -0,0 +1,30 @@
+package com.netsuite.webservices.samples;
+
+import com.netsuite.suitetalk.client.v2019_2.WsClient;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.rmi.RemoteException;
+
+/**
+ * <p>Factory for creating fully initialized web services client.</p>
+ * <p>© 2019 NetSuite Inc. All rights reserved.</p>
+ */
+public final class WsClientFactory {
+
+    private WsClientFactory() {
+    }
+
+    public static WsClient getWsClient(Properties properties, URL endpointUrl) throws MalformedURLException, RemoteException {
+        WsClient client = new WsClient(endpointUrl == null ? properties.getWebServicesUrl() : endpointUrl);
+        if (properties.isTbaRequired()) {
+            System.out.println("#######isTbaRequired: "+properties.isTbaRequired());
+            client.setTokenPassport(properties.getTokenPassport());
+        } else {
+            System.out.println("###############");
+            client.setRequestLevelCredentials(properties.getPassport());
+            client.setApplicationId(properties.getApplicationId());
+        }
+        return client;
+    }
+}
